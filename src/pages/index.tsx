021a5e251830294a68491ffc6401b130f55c3faa
André Loureiro <contato.loureiro1@gmail.com>
2021-07-03 23:24:54 -0300

    Comecando about

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from "next/head";
 
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | Dr.Alda Calixto</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <div className={styles.about}>
+          <div>
+            <img src="/images/icon.png" alt="Dr.Alda Calixto" />
+            <h1>Sobre a clínica</h1>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco{" "}
+          </p>
+        </div>
+      </main>
     </>
   );
 }
