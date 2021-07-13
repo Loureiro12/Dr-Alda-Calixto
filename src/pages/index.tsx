753d52b6e15a975f90480d87191b7ba88db21d89
André Loureiro <contato.loureiro1@gmail.com>
2021-07-12 22:16:25 -0300

    Criando footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -236,32 +236,48 @@ export default function Home() {
             loading="lazy"
           ></iframe>
         </div>
+      </main>
 
-        <div className={styles.footer}>
-          <div className={styles.contanteFooter}>
-            <div>Alda Calixto</div>
-            <div>Entre em contato conosco!</div>
+      <div className={styles.footer}>
+        <div className={styles.contanteFooter}>
+          <div className={styles.links}>
+            <h3 style={{ marginBottom: 10 }}>Alda Calixto</h3>
+            <a href="">Sobre a clínica</a>
+            <br />
+            <a href="">Nossa equipe</a>
+            <br />
+            <a href="">Nossos planos</a>
+          </div>
 
-            <div>
-              <div className={styles.itensFooter}>
-                <img src="/images/instagram.png" alt="Dr.Alda Calixto" />
-                <p>Dra.AldaCalixto</p>
-              </div>
+          <div className={styles.button}>
+            <img src="/images/callWhats.png" alt="Dr.Alda Calixto" />
+            Entre em contato conosco!
+          </div>
 
-              <div className={styles.itensFooter}>
-                <img src="/images/call.png" alt="Dr.Alda Calixto" />
-                <p>34740-2907</p>
-              </div>
+          <div>
+            <div className={styles.itensFooter}>
+              <img src="/images/instagram.png" alt="Dr.Alda Calixto" />
+              <p>Dra.AldaCalixto</p>
+            </div>
 
-              <div className={styles.itensFooter}>
-                <img src="/images/whatsApp.png" alt="Dr.Alda Calixto" />
-                <p>(31) 9 9844-5095</p>
-              </div>
+            <div
+              className={styles.itensFooter}
+              style={{ marginBottom: 20, marginTop: 20 }}
+            >
+              <img src="/images/call.png" alt="Dr.Alda Calixto" />
+              <p>34740-2907</p>
+            </div>
+
+            <div className={styles.itensFooter}>
+              <img src="/images/whatsApp.png" alt="Dr.Alda Calixto" />
+              <p>(31) 9 9844-5095</p>
             </div>
           </div>
-          <p>Alda Calixto — 2021 © Todos os direitos reservados</p>
         </div>
-      </main>
+        <p className={styles.rights}>
+          Alda Calixto — 2021 © Todos os direitos reservados
+        </p>
+      </div>
     </>
   );
 }
