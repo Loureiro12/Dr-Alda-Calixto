aef3715d1aabef2492e1c0815420864a9a32e223
André Loureiro <contato.loureiro1@gmail.com>
2021-07-24 16:22:42 -0300

    [André L] Enviando dados para o banco

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -3,7 +3,24 @@ import { query as q } from "faunadb";
 import { fauna } from "../../services/fauna";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default function (req, res) {
-  const { name } = req.body;
-  console.log({ data: { name } });
+export default async function (req, res) {
+  const { name, email, phone } = req.body;
+
+  try {
+    await fauna.query(
+      q.If(
+        q.Not(
+          q.Exists(
+            q.Match(q.Index("user_by_email"), q.Casefold(req.body.email))
+          )
+        ),
+        q.Create(q.Collection("users"), { data: { name, email, phone } }),
+        q.Update(q.Index("user_by_email"), q.Casefold(req.body.email))
+      )
+    );
+
+    return true;
+  } catch {
+    return false;
+  }
 }
